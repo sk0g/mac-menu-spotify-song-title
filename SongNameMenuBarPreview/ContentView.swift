3ac1c119df729592d9bb82559250368562926515
sk0g <ani.mehta@mail.com>
2021-08-24 20:48:03 +1000

    Initial Commit

---
+++ SongNameMenuBarPreview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SongNameMenuBarPreview
+//
+//  Created by Ani Mehta on 24/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SongNameMenuBarPreview/SongNameMenuBarPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongNameMenuBarPreviewApp.swift
+//  SongNameMenuBarPreview
+//
+//  Created by Ani Mehta on 24/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SongNameMenuBarPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

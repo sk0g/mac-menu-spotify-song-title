bd063f64a15fe537cb5de8d4e445b43e0ddb54a8
sk0g <ani.mehta@mail.com>
2021-08-24 23:14:15 +1000

    Get the song name

--- SongNameMenuBarPreview/ContentView.swift
+++ SongNameMenuBarPreview/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(nowPlaying()).padding()
     }
 }
 
@@ -18,4 +17,28 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func nowPlaying() -> String {
+    let script = """
+    tell application "Spotify"
+        set cstate to current track's name & " - " & current track's artist
+        return cstate
+    end tell
+    """
+
+    if let scriptObject = NSAppleScript(source: script) {
+        var error: NSDictionary?
+        let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+        if (error != nil) {
+            print("xsc error: \(String(describing:error))")
+            return "Ran into error"
+        } else {
+            let value = output.stringValue ?? "Not found"
+            print(value)
+            return value
+        }
+    }
+
+    return "Nothing found?"
 }
---
+++ SongNameMenuBarPreview/Script.sh
@@ -0,0 +1,4 @@
+tell application "Spotify"
+    set cstate to current track's name & " - " & current track's artist
+    return cstate
+end tell
